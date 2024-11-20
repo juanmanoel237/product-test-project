ca8e12587d458fb829ae1f8eda6c4e92b7ba12c9
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-11-20 09:23:27 +0100

    Mise en place du serveur

---
+++ api/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const socketIo = require('socket.io')
+const dotenv = require('dotenv');
+const http = require('http');
+const { log } = require('console');
+
+dotenv.config()
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+io.on('connection', (socket)=>{
+    console.log('Un utilisateur est connecté');
+    socket.on('disconnect', ()=>{
+        console.log('Utilisateur déconnecté');
+        
+    })
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connecté'))
+    .catch((err) => console.log('MongoDB erreur:', err))
+
+server.listen(PORT, () => {
+    console.log(`Server tourne sur le port ${PORT}`)
+})
